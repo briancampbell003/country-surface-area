92bd7add93c0c8d783c6bd1d76c260750d00a44d
Brian Campbell <brian.campbell003@gmail.com>
2023-08-22 09:18:57 -0500

    outline

--- surface-area.py
+++ surface-area.py
@@ -1,5 +1,14 @@
+@task
 #Extract data from source files
 
+@task
 #Calculate rough estimates for surface area
 
+@task
 #Load results into new database file
+
+
+@flow
+    task1
+    task2
+    task3
